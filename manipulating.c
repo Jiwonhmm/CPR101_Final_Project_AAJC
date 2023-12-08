b53632b8508629d565f4ac824fdddd7da925e2a5
CalebG710 <146390404+CalebG710@users.noreply.github.com>
2023-12-08 09:19:57 -0500

    Add files via upload

---
+++ manipulating.c
@@ -0,0 +1,61 @@
+//MANIPULATING SOURCE
+#define _CRT_SECURE_NO_WARNINGS
+#define BUFFER_SIZE 80
+#include "manipulating.h"
+
+void manipulating() {
+		/* Version 1 */
+	void manipulating(void); {
+			print(" * **Start of Concatenating String Demo * **\n");
+			char string1[BUFFER_SIZE];
+			char string2[BUFFER_SIZE];
+
+			do {
+
+				printf("Type the 1st string (q - to quit) :\n");
+					fgets(string1, BUFFER_SIZE, stdin);
+				string1[strlen(string1) - 1] = '\0';
+				if ((strcmp(string1, "q") != 0)) {
+					printf("Type the 2nd string:\n");
+					fgets(string2, BUFFER_SIZE, stdin);
+					string2[strlen(string2) - 1] = '\0';
+					strcat(string1, string2);
+					printf("Concatenated string is \'%s\'\n", string1);
+
+				}
+
+			} while (strcmp(string1, "q") != 0);
+			printf(" * **End of Concatenating Strings Demp * **\n\n");
+
+
+
+
+			/* Version 2 */
+			printf("*** Start of Comparing String Demo ***\n");
+			char compare1[BUFFER_SIZE];
+			char compare2[BUFFER_SIZE];
+			int result;
+			do {
+				printf("Type the 1st string to compare (q - to quit):\n");
+				fgets(compare1, BUFFER_SIZE, stdin);
+				compare1[strlen(compare1) - 1] = '\0';
+				if (strcmp(compare1, "q") != 0) {
+					printf("Type the 2nd string to compare:\n");
+					compare2[strlen(compare2) - 1] = '\0';
+					result = strcmp(compare1, compare2);
+					if (result < 0)
+						printf("\'%s\' string is less than \'%s\'\n", compare1, compare2);
+					else if (result == 0)
+						printf("\'%s\' string is equal to \'%s\'\n", compare1, compare2);
+					else
+						printf("\'%s\' string is greater than \'%s\'\n", compare1, compare2);
+
+
+				}
+
+			} while (strcmp(compare1, "q") != 0);
+			printf("*** End of Cpmparing Strings Demo ***\n\n");
+
+
+			
+}
---
+++ manipulating.h
@@ -0,0 +1,15 @@
+/*
+Caleb Giammarresi - ID#111487237 
+cgiammarresi@myseneca.ca
+CPR101 - Final Project
+11 / 3 / 2023
+maniplulating.h
+*/
+
+
+#ifndef _MANIPULATING_H_
+#define _MANIPULATING_H_
+#include <stdio.h>
+#include <string.h>
+void manipulating(void);
+#endif
